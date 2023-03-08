db6440683b4b3eb6acb4eeaa34c24642dd8eb7f1
Linux Rustling <109832568+TuxMC-sys@users.noreply.github.com>
2023-03-08 15:29:04 -0500

    Update main.py
    Final

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+
+from math import sqrt
+
 print("Which length do you need for the right triangle")
 
 while True:
@@ -8,19 +11,19 @@ while True:
     if userInput == "H":
         angle = input("What is the angled edge for your shape: ")
         width = input("What is the width for your shape: ")
-        hOut = float(width)/float(angle)
+        hOut = sqrt((float(angle)**2)-float(width)**2)
         print(hOut)
     elif userInput == "W":
         height = input("What is the height for your shape:")
         angle = input("What is the angled edge for your shape: ")
-        wOut = float(height)/float(angle)
+        wOut = sqrt((float(angle)**2)-(float(height)**2))
         print(wOut)
     elif userInput == "A":
         height = input("What is the height for your shape:")
         width = input("What is the width for your shape: ")
-        aOut = float(height)/float(width)
+        aOut = sqrt((float(height)*float(height))+(float(width) * float(width)))
         print(aOut)
-    elif userInput == "Q":
+    elif userInput == 'Q':
         break
     else:
         print("Check your input")
