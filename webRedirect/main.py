f99cc6cfbc5dab7b51b3b2ccc95976d6cb07c2f8
Lynn <109832568+TuxMC-sys@users.noreply.github.com>
2024-03-01 23:32:59 -0500

    Proof of concept for web redirect

--- webRedirect/main.py
+++ webRedirect/main.py
@@ -3,10 +3,8 @@ from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 
 website_list = json.load(open("websiteRedirectList.json", "r"))
-print(website_list['0'])
 app = FastAPI()
 
 @app.get("/{websiteToRedirect}", response_class=RedirectResponse)
 async def root(websiteToRedirect):
-    print(websiteToRedirect)
     return website_list[websiteToRedirect]
